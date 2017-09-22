5f64cf0a4f873b43fec085dc4d444f711c5ee46e
Alonso Trejo <h.alonsotrejo@gmail.com>
2017-09-22 12:00:54 -0500

    Inicio Proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app     = express();
+
+/*app.get('/', (req, res) => {
+  res.end('works!');
+});*/
+
+app.use(express.static('public'));
+
+app.listen('3000' , () =>{
+  console.log('servidor iniciado');
+});
